d1d86b2af79f439141d3a92d918f08a5ebe245d6
maulidacitraa <maulida.ellis@gmail.com>
2024-12-15 20:11:03 +0800

    commit

---
+++ node.java
@@ -0,0 +1,13 @@
+public class node {
+    String namaFilm;
+    int durasi;
+    String namaBioskop;
+    node next;
+
+    node(String namaFilm, int durasi, String namaBioskop) {
+        this.namaFilm = namaFilm;
+        this.durasi = durasi;
+        this.namaBioskop = namaBioskop;
+        this.next = next;
+    }
+}
